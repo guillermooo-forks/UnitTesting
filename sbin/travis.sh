12f11de7bcbc81b62e86bdb0ae7d1c38f3711cf8
guillermooo <guillermo.lopez@outlook.com>
2017-12-16 11:54:57 +0100

    vs

--- sbin/travis.sh
+++ sbin/travis.sh
@@ -52,7 +52,7 @@ Bootstrap() {
         fi
 
         echo "download UnitTesting tag: $UNITTESTING_TAG"
-        git clone --quiet --depth 1 "$UT_URL" "$UT_PATH"
+        git clone --quiet --depth 1 --branch "vs" "$UT_URL" "$UT_PATH"
         git -C "$UT_PATH" rev-parse HEAD
         echo
     fi
